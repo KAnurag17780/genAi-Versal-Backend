4af1c10bf41dad88826561d4736333c184c492a8
= <kanurag17780@gmail.com>
2026-07-11 21:13:14 +0530

    Configured Versal

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 require("dotenv").config()
 const connectToDB = require("../src/config/database")
 const app = require("../src/app")
+const serverless = require("serverless-http")
 
 connectToDB()
 
-module.exports = app
+module.exports = serverless(app)
