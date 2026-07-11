bce0f694e82a2e9da6080174daf572468f109878
= <kanurag17780@gmail.com>
2026-07-11 23:09:31 +0530

    fix: remove serverless-http, export Express app directly for Vercel runtime

--- api/index.js
+++ api/index.js
@@ -1,8 +1,7 @@
 require("dotenv").config()
 const connectToDB = require("../src/config/database")
 const app = require("../src/app")
-const serverless = require("serverless-http")
 
 connectToDB()
 
-module.exports = serverless(app)
+module.exports = app
